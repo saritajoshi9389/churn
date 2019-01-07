5ffa686d8c3a0c68fca5ba4ee38613c7f726d444
anela <anela.chan@hbo.com>
2019-01-07 17:41:01 +0000

    Job for new table

--- features/subscription/query.py
+++ features/subscription/query.py
@@ -1,96 +1,43 @@
-def get_sub_sql(query_dt='', max_expiration_dt='', after_expiration_dt=''):
+def get_sub_sql(query_dt='', month_later_dt=''):
     return """
     select
       query_date.user_id,
-
-      case
-          when last_reconnect_date is null then 0
-          else 1
-      end as has_disconnected,
-      case
-          when last_disconnect_date is null then 0
-          else datediff(last_disconnect_date, first_purchase_date)
-      end as previous_days_active,
-      case
-          when last_reconnect_date is null then
-              datediff(to_date('{query_date}', 'yyyy-MM-dd'), first_purchase_date)
-          else
-             datediff(to_date('{query_date}', 'yyyy-MM-dd'), last_reconnect_date)
-      end as current_days_active,
-      case
-          when last_reconnect_date is null then
-              datediff(to_date('{query_date}', 'yyyy-MM-dd'), first_purchase_date)
-          else
-             datediff(last_disconnect_date, first_purchase_date)  + datediff(to_date('{query_date}', 'yyyy-MM-dd'), last_reconnect_date)
-      end as total_days_active,
-
-      -- user counts as churned if churned any time from expiration
-      max(case when later_dates.current_subscription_state = 3 then 1 else 0 end) churned
+      purchase_n as months_subscribing,
+      has_disconnected,
+      case when subsequent_churn.user_id is null then 0 else 1 end as churned
     from
     (
       select
-          external_user_id user_id,
-          current_subscription_state,
-          current_subscription_start_date,
-          current_subscription_end_date,
-          first_purchase_date,
-          last_conversion_date,
-          last_reconnect_date,
-          last_disconnect_date
+          user_id,
+          date_format(subscription_expire_date, 'yyyy-MM-dd') expire_dt,
+          purchase_n,
+          case when datediff(subscription_start_date, first_purchase_date) - (purchase_n * 30) > 100 then 1 else 0 end has_disconnected
       from
          sub_table
       where
         dt = '{query_date}'
-        -- exclude those mysteriously active but expired
-        and date_format(current_subscription_end_date, 'yyyy-MM-dd') >= '{query_date}'
-        -- only with one-month expiration terms
-        and date_format(current_subscription_end_date, 'yyyy-MM-dd') <= '{max_expiration_date}'
-        and current_subscription_state = 2
-        and external_user_id is not null
+        and subscription_type = 'monthly paying'
+        and subscription_term <= 31
+        and subscription_active = 1
     ) query_date
 
-    inner join
+    left outer join
 
     (
       select
-        dt,
-        external_user_id user_id,
-        current_subscription_state
+        user_id,
+        dt
       from
-         sub_table
+        sub_table
       where
-        dt between '{query_date}' and '{after_expiration}'
-        and current_subscription_state in (2, 3)
-        and external_user_id is not null
+        dt between '{query_date}' and '{month_later}'
+        and subscription_active = 1
+        and subscription_drop = 1
 
-    ) later_dates
+    ) subsequent_churn
 
     on
-      query_date.user_id = later_dates.user_id
-    where
-      later_dates.dt >= date_format(query_date.current_subscription_end_date, 'yyyy-MM-dd')
-    group by
-      query_date.user_id,
-      case
-          when last_reconnect_date is null then 0
-          else 1
-      end,
-      case
-          when last_disconnect_date is null then 0
-          else datediff(last_disconnect_date, first_purchase_date)
-      end,
-      case
-          when last_reconnect_date is null then
-              datediff(to_date('{query_date}', 'yyyy-MM-dd'), first_purchase_date)
-          else
-             datediff(to_date('{query_date}', 'yyyy-MM-dd'), last_reconnect_date)
-      end,
-      case
-          when last_reconnect_date is null then
-              datediff(to_date('{query_date}', 'yyyy-MM-dd'), first_purchase_date)
-          else
-             datediff(last_disconnect_date, first_purchase_date)  + datediff(to_date('{query_date}', 'yyyy-MM-dd'), last_reconnect_date)
-      end
+      query_date.user_id = subsequent_churn.user_id
+      and query_date.expire_dt = subsequent_churn.dt
     """.format(query_date=query_dt,
-               max_expiration_date=max_expiration_dt,
-               after_expiration=after_expiration_dt)
+               month_later=month_later_dt)
