10a2abda03b97fa3cc8f7b795222f90521228d7c
anela <anela.chan@hbo.com>
2019-01-14 16:45:49 +0000

    Change just taco so all 'appletv'

--- features/session/query.py
+++ features/session/query.py
@@ -6,11 +6,7 @@ def get_session_sql(query_dt='', days_back=28):
     select
         user_id,
         session_id,
-        case
-            when device_code = 'taco' then 'appletv'
-            when device_code = 'appletv' then 'appletv_classic'
-            else device_code
-        end as device_code,
+        case when device_code = 'taco' then 'appletv' else device_code end as device_code,
         min(cast(event_timestamp as timestamp)) session_start,
         max(cast(event_timestamp as timestamp)) session_end
     from
@@ -22,11 +18,7 @@ def get_session_sql(query_dt='', days_back=28):
     group by
         user_id,
         session_id,
-        case
-            when device_code = 'taco' then 'appletv'
-            when device_code = 'appletv' then 'appletv_classic'
-            else device_code
-        end
+        case when device_code = 'taco' then 'appletv' else device_code end
     """.format(dt_start=utils.dt_start(query_dt, days_back),
                query_dt=query_dt)
 
